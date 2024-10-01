ff4e26106a59c7cae60639cd9a2544441ef068e3
HariharanIT <ghariharanit@gmail.com>
2024-10-01 18:52:04 +0530

    Fixed css and clientid setting

--- src/subComponents/FallbackLogo.tsx
+++ src/subComponents/FallbackLogo.tsx
@@ -102,7 +102,7 @@ export default function FallbackLogo(
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: $config.VIDEO_AUDIO_TILE_COLOR,
+    backgroundColor: '#333',
     justifyContent: 'center',
   },
   // activeSpeakerBg: {
