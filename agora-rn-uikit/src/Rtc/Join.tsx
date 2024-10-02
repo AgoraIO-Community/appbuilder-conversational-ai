0c3ce886e3b48b7dbba7fc352e7810693bee8df1
nitte93 <nitte.tiwari1993@gmail.com>
2024-10-02 14:06:24 +0530

    updated join ref to react state

--- agora-rn-uikit/src/Rtc/Join.tsx
+++ agora-rn-uikit/src/Rtc/Join.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useContext, useRef} from 'react';
+import React, {useEffect, useContext, useRef, useState} from 'react';
 import {IRtcEngine} from 'react-native-agora';
 import {ContentStateInterface} from '../Contexts/RtcContext';
 import {DispatchType} from '../Contexts/DispatchContext';
@@ -14,7 +14,7 @@ const Join: React.FC<{
   tracksReady: boolean;
   preventJoin?: boolean;
 }> = ({children, precall, engineRef, uidState, dispatch, tracksReady}) => {
-  let joinState = useRef(false);
+  let [joinState, setJoinState] = useState(false);
   const {rtcProps} = useContext(PropsContext);
 
   const audioRoom = rtcProps?.audioRoom || false;
@@ -24,12 +24,12 @@ const Join: React.FC<{
   //   : null;
 
   useEffect(() => {
-    if (joinState.current && tracksReady && Platform.OS === 'web') {
+    if (joinState && tracksReady ) {
       //@ts-ignore
       engineRef.current.publish();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [tracksReady]);
+  }, [tracksReady, joinState]);
 
   useEffect(() => {
     if (rtcProps?.preventJoin) {
@@ -41,7 +41,8 @@ const Join: React.FC<{
       try {
         console.log('Leaving channel');
         engine.leaveChannel();
-        joinState.current = false;
+        // joinState.current = false;
+        setJoinState(false)
       } catch (err) {
         console.error('Cannot leave the channel:', err);
       }
@@ -118,9 +119,9 @@ const Join: React.FC<{
     }
     async function init() {
       if (!precall) {
-        if (!joinState.current) {
+        if (joinState) {
           await join();
-          joinState.current = true;
+          setJoinState(true)
         } else {
           await leave();
           await join();
